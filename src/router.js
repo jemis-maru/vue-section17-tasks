e9f62ac5b8b2449924d4f481a81bac38ad399ed0
jemis-maru <jemis.maru@bacancy.com>
2022-02-22 18:04:14 +0530

    Deploy to firebase

--- src/router.js
+++ src/router.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachDetails from './pages/coaches/CoachDetails.vue';
-import CoachRegister from './pages/coaches/CoachRegister.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import ReceivedRequest from './pages/requests/ReceivedRequest.vue';
+// import CoachDetails from './pages/coaches/CoachDetails.vue';
+// import CoachRegister from './pages/coaches/CoachRegister.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import ReceivedRequest from './pages/requests/ReceivedRequest.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
 import store from "./store/index.js";
+
+const CoachDetails = () => import('./pages/coaches/CoachDetails.vue');
+const CoachRegister = () => import('./pages/coaches/CoachRegister.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const ReceivedRequest = () => import('./pages/requests/ReceivedRequest.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
 
 const router = createRouter({
     history: createWebHistory(),
